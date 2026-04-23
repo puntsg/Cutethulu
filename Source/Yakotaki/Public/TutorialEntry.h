6c4f99edeb635af4df77bb65c8863b31895f44d6
SergiGBerlanga-DESKTOP-10JPS7V\sergi <sergigonzalezberlanga@gmail.com>
2026-04-23 21:31:12 +0200

    Tutorial UI data moved to data Tables

--- Source/Yakotaki/Public/TutorialEntry.h
+++ Source/Yakotaki/Public/TutorialEntry.h
@@ -10,6 +10,96 @@
 /**
  * 
  */
+UENUM(BlueprintType)
+enum class ETutorialContent : uint8 {
+	TEXT UMETA(DisplayName = "Text"),
+	IMAGE UMETA(DisplayName = "Image"),
+	SPACER UMETA(DisplayName = "Spacer")
+};
+
+USTRUCT(BlueprintType)
+struct YAKOTAKI_API FTutorialImageBlock
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FMargin padding;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	TEnumAsByte<EHorizontalAlignment> horizontalAlignment;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	TEnumAsByte<EVerticalAlignment> verticalAligment;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	bool forceNewLine;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	UTexture2D* image;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FVector2D imageSize;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FLinearColor colourAndOpacity;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	float renderOpacity;
+};
+
+USTRUCT(BlueprintType)
+struct YAKOTAKI_API FTutorialSpacerBlock
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	bool fillEmptySpace;
+	
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FVector2D size;
+};
+
+USTRUCT(BlueprintType)
+struct YAKOTAKI_API FTutorialTextBlock
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FMargin Padding;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	bool forceNewLine;
+	
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FText text;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FTextBlockStyle textStyle;
+
+	//UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	//ETextJustify justification;
+};
+
+
+USTRUCT(BlueprintType)
+struct YAKOTAKI_API FTutorialContent
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	ETutorialContent tutorialContent;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FTutorialTextBlock textContent;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FTutorialImageBlock imageContent;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FTutorialSpacerBlock spacer;
+};
+
+
 USTRUCT(BlueprintType)
 struct YAKOTAKI_API FTutorialEntry : public FTableRowBase
 {
@@ -17,4 +107,7 @@ struct YAKOTAKI_API FTutorialEntry : public FTableRowBase
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	FString tutorialName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FTutorialContent> tutorialContents;
 };
